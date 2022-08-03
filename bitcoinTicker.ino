14f8bc3a6a19439d4a2d7986a8ef3792893ccdcc
Agustin Suarez <suarez.agu@gmail.com>
2022-08-02 22:19:15 -0400

    testing api limiting

--- bitcoinTicker.ino
+++ bitcoinTicker.ino
@@ -12,6 +12,8 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+
+NTPClient timeClient(ntpUDP);
 const char* ssid = SECRET_SSID;
 const char* password = SECRET_WIFI_PASSWORD;
 
@@ -20,7 +22,7 @@ const int httpsPort = 443;
 const String url = "http://api.coindesk.com/v1/bpi/currentprice/BTC.json";
 const String historyURL = "http://api.coindesk.com/v1/bpi/historical/close.json";
 const String cryptoCode = "BTC";
-
+const String INTERVAL = 5;
 WiFiClient client;
 HTTPClient http;
 
@@ -31,6 +33,8 @@ String timeStamp;
 
 void setup() {
   Serial.begin(115200);
+  timeClient.begin();
+
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -63,90 +67,93 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Connecting to ");
-  Serial.println(url);
+  timeClient.update();
 
-  http.begin(url);
-  int httpCode = http.GET();
-  StaticJsonDocument<2000> doc;
-  DeserializationError error = deserializeJson(doc, http.getString());
+  if(timeClient.getEpochTime() % INTERVAL == 0) {
+    Serial.print("Connecting to ");
+    Serial.println(url);
 
-  if (error) {
-    Serial.print(F("deserializeJson Failed"));
-    Serial.println(error.f_str());
-    delay(2500);
-    return;
+    http.begin(url);
+    int httpCode = http.GET();
+    StaticJsonDocument<2000> doc;
+    DeserializationError error = deserializeJson(doc, http.getString());
+
+    if (error) {
+      Serial.print(F("deserializeJson Failed"));
+      Serial.println(error.f_str());
+      delay(2500);
+      return;
+    }
+
+    Serial.print("HTTP Status Code: ");
+    Serial.println(httpCode);
+
+    String BTCUSDPrice = doc["bpi"]["USD"]["rate_float"].as<String>();
+    String lastUpdated = doc["time"]["updated"].as<String>();
+    http.end();
+
+    Serial.print("Getting history...");
+    StaticJsonDocument<2000> historyDoc;
+    http.begin(historyURL);
+    int historyHttpCode = http.GET();
+    DeserializationError historyError = deserializeJson(historyDoc, http.getString());
+
+    if (historyError) {
+      Serial.print(F("deserializeJson(History) failed"));
+      Serial.println(historyError.f_str());
+      delay(2500);
+      return;
+    }
+
+    Serial.print("History HTTP Status Code: ");
+    Serial.println(historyHttpCode);
+    JsonObject bpi = historyDoc["bpi"].as<JsonObject>();
+    double yesterdayPrice;
+    for (JsonPair kv : bpi) {
+      yesterdayPrice = kv.value().as<double>();
+    }
+
+    Serial.print("BTCUSD Price: ");
+    Serial.println(BTCUSDPrice.toDouble());
+
+    Serial.print("Yesterday's Price: ");
+    Serial.println(yesterdayPrice);
+
+    bool isUp = BTCUSDPrice.toDouble() > yesterdayPrice;
+    double percentChange;
+    if (isUp) {
+      percentChange = ((BTCUSDPrice.toDouble() - yesterdayPrice) / yesterdayPrice) * 100;
+    } else {
+      percentChange = ((yesterdayPrice - BTCUSDPrice.toDouble()) / yesterdayPrice) * 100;
+    }
+
+    Serial.print("Percent Change: ");
+    Serial.println(percentChange);
+
+    //Display Header
+    display.clearDisplay();
+    display.setTextSize(1);
+    printCenter("BTC/USD", 0, 0);
+
+    //Display BTC Price
+    display.setTextSize(2);
+    printCenter("$" + BTCUSDPrice, 0, 25);
+
+    //Display 24hr. Percent Change
+    String dayChangeString = "24hr. Change: ";
+    if (isUp) {
+      percentChange = ((BTCUSDPrice.toDouble() - yesterdayPrice) / yesterdayPrice) * 100;
+    } else {
+      percentChange = ((yesterdayPrice - BTCUSDPrice.toDouble()) / yesterdayPrice) * 100;
+      dayChangeString = dayChangeString + "-";
+    }
+    display.setTextSize(1);
+    dayChangeString = dayChangeString + percentChange + "%";
+    printCenter(dayChangeString, 0, 55);
+    display.display();
+
+    http.end();
   }
-
-  Serial.print("HTTP Status Code: ");
-  Serial.println(httpCode);
-
-  String BTCUSDPrice = doc["bpi"]["USD"]["rate_float"].as<String>();
-  String lastUpdated = doc["time"]["updated"].as<String>();
-  http.end();
-
-  Serial.print("Getting history...");
-  StaticJsonDocument<2000> historyDoc;
-  http.begin(historyURL);
-  int historyHttpCode = http.GET();
-  DeserializationError historyError = deserializeJson(historyDoc, http.getString());
-
-  if (historyError) {
-    Serial.print(F("deserializeJson(History) failed"));
-    Serial.println(historyError.f_str());
-    delay(2500);
-    return;
-  }
-
-  Serial.print("History HTTP Status Code: ");
-  Serial.println(historyHttpCode);
-  JsonObject bpi = historyDoc["bpi"].as<JsonObject>();
-  double yesterdayPrice;
-  for (JsonPair kv : bpi) {
-    yesterdayPrice = kv.value().as<double>();
-  }
-
-  Serial.print("BTCUSD Price: ");
-  Serial.println(BTCUSDPrice.toDouble());
-
-  Serial.print("Yesterday's Price: ");
-  Serial.println(yesterdayPrice);
-
-  bool isUp = BTCUSDPrice.toDouble() > yesterdayPrice;
-  double percentChange;
-  if (isUp) {
-    percentChange = ((BTCUSDPrice.toDouble() - yesterdayPrice) / yesterdayPrice) * 100;
-  } else {
-    percentChange = ((yesterdayPrice - BTCUSDPrice.toDouble()) / yesterdayPrice) * 100;
-  }
-
-  Serial.print("Percent Change: ");
-  Serial.println(percentChange);
-
-  //Display Header
-  display.clearDisplay();
-  display.setTextSize(1);
-  printCenter("BTC/USD", 0, 0);
-
-  //Display BTC Price
-  display.setTextSize(2);
-  printCenter("$" + BTCUSDPrice, 0, 25);
-
-  //Display 24hr. Percent Change
-  String dayChangeString = "24hr. Change: ";
-  if (isUp) {
-    percentChange = ((BTCUSDPrice.toDouble() - yesterdayPrice) / yesterdayPrice) * 100;
-  } else {
-    percentChange = ((yesterdayPrice - BTCUSDPrice.toDouble()) / yesterdayPrice) * 100;
-    dayChangeString = dayChangeString + "-";
-  }
-  display.setTextSize(1);
-  dayChangeString = dayChangeString + percentChange + "%";
-  printCenter(dayChangeString, 0, 55);
-  display.display();
-
-  http.end();
-  delay(10000);
 }
 
 void printCenter(const String buf, int x, int y)
